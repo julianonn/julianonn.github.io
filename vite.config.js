570eb2f1b7cd1113d64a91bbd7e1c7d91a38edfb
julianonn <95244239+julianonn@users.noreply.github.com>
2024-07-19 16:04:16 -0400

    minor text updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        google: resolve(__dirname, 'googlee3af68ee84d3e19d.html'),
+      }
+    }
+  }
 })
