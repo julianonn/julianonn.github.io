eb64138020191d5de139b289da3116c24728dea3
julianonn <julianonn@users.noreply.github.com>
2025-07-16 16:08:42 +0000

    Deploying to gh-pages from @ julianonn/julianonn.github.io@c61ad37a495f7c12f151f6e2178738f8d5a84711 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,42 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-i-started-my-ms-phd-at-uw-madison",
+          title: 'I started my MS/PhD at UW-Madison!',
+          description: "",
+          section: "News",},{id: "news-our-lab-attended-the-midwest-security-workshop-at-purdue",
+          title: 'Our lab attended the Midwest Security Workshop at Purdue.',
+          description: "",
+          section: "News",},{id: "news-our-paper-about-identifying-tech-abuse-in-gdpr-data-exports-got-accepted-to-acm-ccs-2025",
+          title: 'Our paper about identifying tech abuse in GDPR data exports got accepted to...',
+          description: "",
+          section: "News",},];
